ecf6c7d2f85b9ad3fe82ee9a1128184afae57c6c
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-09-02 13:11:47 +0300

    creating the homepage view

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Company, JobPosition, Application
+from .forms import CompanyForm, JobPositionForm, ApplicationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+def homepage(request):
+    return render(request, 'homepage.html')
