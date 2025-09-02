1b330c766194ffb3b476ef8925a6effb8b5741c2
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-09-02 13:15:49 +0300

    creating sign-up.html and login.html

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,9 @@ from django.views.generic import CreateView
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+class SignUpView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/sign-up.html'
