ed3fe466ec6d5881403691e2baaf3e0c56c5e9be
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-09-06 14:46:19 +0300

    adding labels to job position form and application form

--- main_app/forms.py
+++ main_app/forms.py
@@ -14,8 +14,19 @@ class JobPositionForm(forms.ModelForm):
     class Meta:
         model = JobPosition
         fields = ['location', 'salary']
+        labels = {
+            'location': 'Company Location',
+            'salary': 'Salary'
+        }
 
 class ApplicationForm(forms.ModelForm):
     class Meta:
         model = Application
         fields = ['job_position', 'date_applied', 'status', 'notes', 'resume']
+        labels = {
+            'job_position': 'Job Position',
+            'date_applied': 'Date Applied',
+            'status': 'Application Status',
+            'notes': 'Additional Notes',
+            'resume': 'Upload Resume'
+        }
