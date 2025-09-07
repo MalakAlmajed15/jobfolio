fea3c6a59998d587072d950d574903519fc74776
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-09-07 09:47:50 +0300

    delete

--- jobfolio_project/settings.py
+++ jobfolio_project/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5432' ,
     }
 }
 
