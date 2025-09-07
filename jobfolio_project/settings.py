91cdd3ee9f1f081707a5117dd133fa5f68499918
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-09-07 09:27:31 +0300

    fixing an error

--- jobfolio_project/settings.py
+++ jobfolio_project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'jobfolio_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': 'postgres',
         'PASSWORD': os.getenv('DB_PASSWORD'),
