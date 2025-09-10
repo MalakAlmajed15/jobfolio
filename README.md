82f89f597eda48e7721c6d51020086af2b09ff09
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-09-10 10:15:04 +0300

    removing comments

---
+++ README.md
@@ -0,0 +1,37 @@
+# 🎬 Project4: JobFolio
+
+## 📝 Introduction
+
+**JobFolio** 
+
+### Why I Built It
+
+
+---
+
+## 🚀 Getting Started
+
+* 🌐 [Website Link]()
+* 📂 [Planning Materials]()
+
+---
+
+## 🎨 Screenshot
+
+
+---
+
+## 🛠️ Technologies Used
+
+
+---
+
+## 🔮 Future Enhancements
+
+
+
+---
+
+## 🙏 Acknowledgements
+
+* [Google Fonts](https://fonts.google.com/)
--- jobfolio_project/settings.py
+++ jobfolio_project/settings.py
@@ -15,24 +15,15 @@ import os
 from dotenv import load_dotenv
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR/ '.env')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-22r1!*h$ofsd-hjhg9d=uver^0k6a#3yufhb%m-)t$d6x4$31k'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -73,10 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jobfolio_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,9 +75,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -107,10 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -119,14 +99,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -134,12 +107,5 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'homepage'
 LOGOUT_REDIRECT_URL = 'login'
 
-# CLOUDINARY_STORAGE = {
-#     "CLOUD_NAME": os.getenv('CLOUD_NAME'),
-#     "API_KEY": os.getenv('API_KEY'),
-#     "API_SECRET": os.getenv('API_SECRET'),
-# }
-
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
